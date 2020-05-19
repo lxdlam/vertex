8cc297e73749b66ba57391bce287b2407c20f1bb
lxdlam <lxdlam@gmail.com>
2020-05-20 04:39:06 +0800

    Fix: some api changes

--- pkg/command/command.go
+++ pkg/command/command.go
@@ -1,13 +1,12 @@
 package command
 
 import (
-	"github.com/lxdlam/vertex/pkg/container"
 	"github.com/lxdlam/vertex/pkg/protocol"
 )
 
 type Command interface {
 	Name() string
-	Execute(container.Container) (protocol.RedisObject, error)
+	Execute(string) (protocol.RedisObject, error)
 	Validate() bool
 }
 
